c5acd7b31af8e9d78719e492d50889a3209c9ff1
Robertas Baravikov <robka.bar@gmail.com>
2023-12-27 13:23:05 +0200

    start

---
+++ app.jsx
@@ -0,0 +1,36 @@
+const App = () => {
+    return (
+      <>  
+          <img width="150" src="logo.png" alt="Lili-LoLo" />
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">Product</a>
+            <a href="#">Company</a>
+            <a href="#">Contact</a>
+          </nav>
+          <div className="header">
+            </div>
+          <div className="tags">
+            <div className="tag"><h2>About</h2><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat dolore corporis deleniti rem nesciunt soluta reprehenderit magnam similique est quaerat, optio minima eveniet quisquam praesentium dolores nihil iure facere omnis?</p></div>
+            <div className="tag"><h2>Company</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, voluptates? Optio saepe accusamus sint consequatur quibusdam ipsum provident ducimus reprehenderit.</p></div>
+            <div className="tag"><h2>Services</h2><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur odio totam magnam laboriosam harum autem, neque ipsam, quas blanditiis nisi laborum, maxime minus nam explicabo recusandae dicta! Alias ipsam maxime, optio iure dolor incidunt sequi?</p></div>
+          </div>
+          <div className="content-page">
+            <div className="navigation">
+              <h3>Navigation</h3>
+              <a href="#">Home</a>
+              <a href="#">Product</a>
+              <a href="#">Company</a>
+              <a href="#">Contact</a>
+            </div>
+            <h2>Content</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque hic maxime illum aspernatur, minus perspiciatis repellat dignissimos recusandae accusamus cupiditate nulla laudantium unde, blanditiis quod delectus error accusantium doloribus quae!</p>
+            <h3>Sub header</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi mollitia eum optio similique eius perferendis earum sapiente repellat, blanditiis itaque hic quo? Aliquid dolorum reiciendis vel veniam enim suscipit repudiandae, corrupti natus tempore quia? Cum fugiat repellat inventore a quasi dicta, delectus vero, consequuntur debitis quam beatae, voluptates nisi veniam laudantium molestiae? Adipisci quos aut officiis cumque maiores at laborum, sequi ex doloribus dolorum numquam consectetur quaerat quod molestias autem unde ad repudiandae voluptatum itaque aliquam. Libero, voluptates corporis repellendus a distinctio cumque illo qui, eum error sint harum veniam dolores officia mollitia laboriosam! Voluptate odio perspiciatis facere consequatur neque nostrum laborum, quia deleniti sunt earum ut iusto! Molestiae fuga eum enim impedit laudantium pariatur sapiente soluta, veniam, ipsum aut est! Qui aut suscipit temporibus voluptatem deleniti, ullam ut eum quae dolores atque, expedita voluptatibus! Corporis voluptas unde sed velit officia sit nisi, accusantium harum assumenda ullam, maiores laborum alias fuga, quod voluptatum provident enim repudiandae ut praesentium culpa dolorum quisquam veniam! Nihil nemo voluptate excepturi non dolore sint delectus, fuga provident laborum distinctio aperiam voluptatum, deleniti aliquid animi omnis! Perspiciatis fugiat ad id officia dolores temporibus dignissimos est sed! Veritatis, officia illo. Iure voluptatibus exercitationem quam corporis officia enim laborum repudiandae voluptatum adipisci nihil nostrum dignissimos a in ab illum amet odio iste sit repellat, dicta praesentium nam architecto delectus vel. Nobis incidunt eligendi voluptate ut laudantium. Atque ipsa earum nihil optio natus repellat sapiente ipsam beatae fuga accusamus unde magni sequi illum, ullam tempora debitis dolores quod sit impedit quidem minus odio ratione culpa quas? Quidem doloremque nam sed eum nemo veniam deserunt nobis, ex atque corrupti modi et perferendis. Error, repellat eum mollitia voluptatum porro exercitationem eveniet iste. Unde itaque harum, dolorum explicabo, distinctio alias necessitatibus est doloremque ratione pariatur laboriosam deserunt consequatur vero quibusdam, fugit similique?</p>
+            
+          </div>
+      </>
+    );
+  };
+
+  ReactDOM.createRoot(document.getElementById('app')).render(<App />);
